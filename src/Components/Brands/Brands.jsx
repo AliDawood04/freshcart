af9b65ad068ef24af1312e384ff9ff2a21a68aad
AliDawood04 <aliabozieddawood@yahoo.com>
2024-09-10 22:08:51 +0300

    +new

--- src/Components/Brands/Brands.jsx
+++ src/Components/Brands/Brands.jsx
@@ -1,14 +1,42 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import style from './Brands.module.css'
+import axios from 'axios';
 
 export default function Brands() {
 
+  const [brands, setBrands] = useState(null);
 
-
-    
-  return <>
-    
-    <h1 className="text-3xl">Brands</h1>
   
-  </>
+  async function getCategories(){
+    
+    try {
+      let {data}=await axios.get('https://ecommerce.routemisr.com/api/v1/brands')
+      setBrands(data)
+    } catch (error) {
+      console.log(error);
+      
+    }
+  
+  }
+    
+  useEffect(() => {
+    getCategories()
+
+      }, [])
+      
+    return <>
+      <h2 className='pb-5 text-5xl flex justify-center text-mainColor'>Brands</h2>
+
+     <div className='flex flex-wrap'>
+      <div className="w-1/4 p-2">
+
+      {brands?.map((brand,index)=> (<div key={index} className='flex'>
+        <img src={brand.image} alt="" className="w-full "  />
+
+      </div>))}
+
+      </div>
+     </div>
+
+  </>  
 }
--- src/Components/Categories/Categories.jsx
+++ src/Components/Categories/Categories.jsx
@@ -1,10 +1,21 @@
 import React, { useState } from 'react'
 import style from './Categories.module.css'
+import axios from 'axios';
 
 export default function Categories() {
 
+const [state, setstate] = useState();
 
+async function getCategories(){
+  
+  try {
+    let {data}=await axios.get('https://ecommerce.routemisr.com/api/v1/brands')
+  } catch (error) {
+    console.log(error);
+    
+  }
 
+}
     
   return <>
     
