b2d76e0eb2cab398f5c07d04991d7a47ea037718
AliDawood04 <aliabozieddawood@yahoo.com>
2024-09-13 18:57:42 +0300

    new

--- src/Components/Brands/Brands.jsx
+++ src/Components/Brands/Brands.jsx
@@ -1,41 +1,44 @@
 import React, { useEffect, useState } from 'react'
 import style from './Brands.module.css'
 import axios from 'axios';
+import { data } from 'autoprefixer';
 
-export default function Brands() {
+export default function brands() {
 
   const [brands, setBrands] = useState(null);
 
   
-  async function getCategories(){
+  async function getBrands(){
     
     try {
       let {data}=await axios.get('https://ecommerce.routemisr.com/api/v1/brands')
-      setBrands(data)
+      setBrands(data.data)
     } catch (error) {
       console.log(error);
       
     }
   
   }
+   console.log(data);
     
   useEffect(() => {
-    getCategories()
+    getBrands()
 
       }, [])
       
     return <>
       <h2 className='pb-5 text-5xl flex justify-center text-mainColor'>Brands</h2>
 
-     <div className='flex flex-wrap'>
-      <div className="w-1/4 p-2">
+     <div className='flex flex-wrap '>
+      
 
-      {brands?.map((brand,index)=> (<div key={index} className='flex'>
-        <img src={brand.image} alt="" className="w-full "  />
+      {brands?.map((brand,index)=> (<div key={index} className='p-2 w-1/4 product'>
+        <img src={brand.image} alt={brand.name} className="w-full "  />
+        <h2 className='text-mainColor py-2 text-xl'>{brand.name}</h2>
 
       </div>))}
 
-      </div>
+      
      </div>
 
   </>  
--- src/Components/Categories/Categories.jsx
+++ src/Components/Categories/Categories.jsx
@@ -1,25 +1,43 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import style from './Categories.module.css'
 import axios from 'axios';
 
+
 export default function Categories() {
 
-const [state, setstate] = useState();
+const [categories, setCategories] = useState(null);
 
 async function getCategories(){
   
   try {
-    let {data}=await axios.get('https://ecommerce.routemisr.com/api/v1/brands')
+    let {data}=await axios.get('https://ecommerce.routemisr.com/api/v1/categories')
+    setCategories(data.data)
   } catch (error) {
     console.log(error);
     
   }
-
 }
+
+  useEffect(() => {
+   getCategories()
+  }, [])
     
   return <>
     
-    <h1 className="text-3xl">Categories</h1>
+    <h2 className="pb-5 text-5xl flex justify-center text-mainColor">Categories</h2>
+    <div className="flex flex-wrap justify-center">
+      {categories?.map((category,index)=>(
+        <div key={index} className='p-2 w-1/4 product '>
+          <div className='h-[400px]'>
+          <img src={category.image} alt={category.name} className='w-full h-full ' />
+          </div>
+
+          <h2 className='text-mainColor py-2 text-xl'>{category.name}</h2>
+
+          
+         
+      </div>))}
+    </div>
   
   </>
 }
